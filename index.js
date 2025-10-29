e40cdd5232753b07b9d4824fad05db880d8f03c2
Lisandro Andújar <45610536+Lisandr8@users.noreply.github.com>
2025-10-29 19:29:55 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,24 @@
 const hourHand = document.querySelector('.hours-hand');
 const minsHand = document.querySelector('.mins-hand');
-const secondHand = document.querySelector('.secs-hand');
+const secsHand = document.querySelector('.secs-hand');
 const showTime = document.querySelector('h1');
-
 
 function setDate() {
     const now = new Date()
 
-     const seconds = now.getSeconds();
-    const secondsDegrees = ((seconds / 60) * 360) + 90;
-    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+    const secs = now.getSeconds();
+    const secsDegrees = ((secs / 60) * 360) + 90;
+    secsHand.style.transform = `rotate(${secsDegrees}deg)`;
 
     const mins = now.getMinutes();
-    const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
+    const minsDegrees = ((mins / 60) * 360) + ((secs / 60) * 6) + 90;
     minsHand.style.transform = `rotate(${minsDegrees}deg)`;
 
-    const hour = now.getHours();
-    const hourDegrees = ((hour / 12) * 360) + ((mins/60)*30) + 90;
+    const hours = now.getHours();
+    const hourDegrees = ((hours / 12) * 360) + ((mins / 60) * 30) + 90;
     hourHand.style.transform = `rotate(${hourDegrees}deg)`;
 
     showTime.textContent = `${hours} : ${mins} : ${secs}`
 }
 
 setInterval(setDate, 1000)
-
-
